c3308ce87cc10ea93de3c1a3e14471944cf41dcc
mannyavila <mannyavila90@gmail.com>
2023-12-01 17:56:25 -0400

    Primeros pasos e instalacion de bootstrap y axios

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Container } from "react-bootstrap"
+
+function App() {
+
+  return (
+    <>
+      <header className="py-5">
+        <h1>Buscador de Bebidas</h1>
+      </header>
+
+      <Container>
+        
+      </Container>
+    </>
+  )
+}
+
+export default App
