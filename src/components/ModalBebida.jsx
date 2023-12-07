5832d5067d39022134eb4ebe10b28736e0aae6fe
mannyavila <mannyavila90@gmail.com>
2023-12-07 01:01:43 -0400

    Modal finalizado con las recetas

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -1,14 +1,40 @@
-import { Modal } from "react-bootstrap"
+import { Image, Modal } from "react-bootstrap"
 import useBebidas from "../hooks/useBebidas"
 
 const ModalBebida = () => {
 
-    const {modal, handleModalClick} = useBebidas()
+    const {modal, handleModalClick, receta} = useBebidas()
+
+    const mostrarIngredientes = () => {
+      let ingredientes = []
+
+      for (let i = 1; i < 16; i++) {
+        if (receta[`strIngredient${i}`]) {
+          ingredientes.push(
+            <li>{receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}</li>
+          )
+        }
+      }
+      return ingredientes
+    }
 
   return (
     <Modal show={modal} onHide={handleModalClick}>
+      <Image 
+        src={receta.strDrinkThumb} 
+        alt={`Imagen receta ${receta.strDrink}`}
+      />
+
+      <Modal.Header>
+        <Modal.Title>{receta.strDrink}</Modal.Title>
+      </Modal.Header>
       <Modal.Body>
-        cuerpo modal
+        <div className="p-3">
+          <h2>Instrucciones</h2>
+          {receta.strInstructions}
+          <h2>Ingredientes y Cantidad</h2>
+          {mostrarIngredientes()}
+        </div>
       </Modal.Body>
     </Modal>
   )
